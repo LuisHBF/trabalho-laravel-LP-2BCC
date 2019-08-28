d26242e8a5f1a7c5abf8caad8ed80d43a7b35d40
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-28 19:06:09 -0300

    Criando controller para Venda

---
+++ app/Http/Controllers/VendaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Funcionario;
+use Illuminate\Http\Request;
+
+class VendaController extends Controller
+{
+
+    public function index(){
+
+    }
+}
